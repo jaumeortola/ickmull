ab6748a498553a578b322167b84704fada452bfc
kfahlgren <kfahlgren@localhost>
2009-12-21 20:37:48 +0000

    Basic project setup for automated testing

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='ickmull',
+      version=version,
+      description="Ickmull provides tools and testing for the transformation of XHTML into ICML using XSLT.",
+      long_description="""\
+      Ickmull is a development of John W Maxwell at the Canadian Centre for Studies in Publishing at Simon Fraser University 
+      See http://thinkubator.ccsp.sfu.ca/wikis/xmlProduction/Home for more details""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='icml xslt indesign',
+      author='John Maxwell',
+      author_email='jmax@sfu.ca',
+      url='http://thinkubator.ccsp.sfu.ca/wikis/xmlProduction/Home',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'lxml >= 2.2'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
